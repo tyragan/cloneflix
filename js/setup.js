1da9ddef28e99cdc83f1fc864f2dc1e4b9ceec04
Silvio Guilherme <multilayrs394@gmail.com>
2020-10-21 00:37:14 -0200

    fim do projeto

---
+++ js/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+function assistido(){
+    document.getElementsByid("assistir").style.background-color='white';
+    
+}
